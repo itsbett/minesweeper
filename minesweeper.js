3069ab046bcc46def55b5a6f7410fe82d77a83e1
Brett <langfordbrett@gmail.com>
2019-04-15 19:36:05 -0500

    updated display and tiles

--- minesweeper.js
+++ minesweeper.js
@@ -1,30 +1,101 @@
-const canvas = document.getElementById('canvas')
-const context = canvas.getContext('2d')
+const container = document.getElementById('container')
 let mousePosition = null
-canvas.addEventListener('mousedown', mouseClick, false)
-canvas.addEventListener('mousemove', moveMouse, false)
 let mineSweeper = 'haha'
 let grass = new Image()
-grass.src = 'images/grass_tile_mid_no_border.png'
+grass.src = 'tiles/grass_center.png'
 
 class Minesweeper {
   constructor (row, col, mines) {
     this.row = row
     this.col = col
+    this.canvas = this.buildCanvas()
+    this.context = this.canvas.getContext('2d')
+    this.canvas.addEventListener('mousedown', mouseClick, false)
+    this.canvas.addEventListener('mousemove', moveMouse, false)
     this.mineList = randomMines(mines, row, col)
     this.board = new Board(row, col, () => this.mineList.pop() === 1)
     this.startGame()
   }
   startGame () {
-    canvas.setAttribute('width', this.row * 32)
-    canvas.setAttribute('height', this.col * 32)
-    this.draw()
+    // this.draw()
   }
   endGame () {
     console.log('you lose, fuckboi')
   }
+  buildCanvas () {
+    for (let i = 0; i < this.row + 2; i++) {
+      let div = document.createElement('div')
+      for (let j = 0; j < this.col + 5; j++) {
+        if (i === 1 && j === 1) {
+          let div2 = document.createElement('div')
+          let canvas1 = document.createElement('canvas')
+          canvas1.setAttribute('id', 'generatedCanvas')
+          canvas1.height = 32 * this.col
+          canvas1.width = 32 * this.row
+          canvas1.style.backgroundColor = 'white'
+          div2.appendChild(canvas1)
+          div.appendChild(div2)
+        } else {
+          let div2 = document.createElement('div')
+          let tilepos = this.getTile(i, j)
+          div2.setAttribute('id', tilepos)
+          div.appendChild(div2)
+        }
+      }
+      container.appendChild(div)
+    }
+    return document.getElementById('generatedCanvas')
+  }
+  getTile (i, j) {
+    if (j > this.col + 1) { // should we draw the cliff?
+      if (i === 0) {
+        if (j === this.col + 4) {
+          return 'rockbotleft'
+        }
+        return 'rockleft'
+      }
+      if (i === this.row + 1) {
+        if (j === this.col + 4) {
+          return 'rockbotright'
+        }
+        return 'rockright'
+      }
+      if (j === this.col + 4) {
+        return 'rockbotmid'
+      }
+      return 'rockmid'
+    }
+    if (i === 1 && j === 1) {
+      return 'canvascontainer'
+    }
+    if (i === 0) { // are we on the left side?
+      if (j === 0) {
+        return 'topleft' // is this the top left tile?
+      }
+      if (j === this.col + 1) { // is this bottom left tile?
+        return 'botleft'
+      }
+      return 'left' // otherwise it's a left tile
+    }
+    if (j === 0) { // are we on top side?
+      if (i === this.row + 1) { // is this top right tile?
+        return 'topright'
+      }
+      return 'top'
+    }
+    if (j === this.col + 1) { // is this right side?
+      if (i === this.row + 1) { // is this bottom right tile?
+        return 'botright'
+      }
+      return 'bottom'
+    }
+    if (i === this.row + 1) {
+      return 'right'
+    }
+    return 'center'
+  }
   draw () {
-    let container = document.getElementById('container')
+    let container1 = document.getElementById('container1')
     for (let i = 0; i < this.row; i++) {
       let r = document.createElement('div')
 
@@ -41,24 +112,25 @@ class Minesweeper {
         r.appendChild(c)
       }
       r.setAttribute('class', 'left')
-      container.appendChild(r)
+      container1.appendChild(r)
     }
   }
 }
 
 class Node {
-  constructor (x, y, isMine) {
+  constructor (x, y, isMine, tile) {
     this.x = x
     this.y = y
     this.escavated = false
     this.isMine = isMine
+    this.tile = tile
     this.surroundingMines = null
     this.selected = false
   }
 
   click () {
     if (!this.escavated) {
-      context.font = '30px Arial'
+      mineSweeper.context.font = '30px Arial'
       this.escavated = true
       if (this.isMine) {
         this.surroundingMines = this.getSurroundingMines() // delete this later
@@ -75,17 +147,17 @@ class Node {
 
   draw () {
     if (this.selected) {
-      context.strokeRect(this.x * 32 + 1, this.y * 32 + 1, 29, 29)
+      mineSweeper.context.strokeRect(this.x * 32 + 1, this.y * 32 + 1, 29, 29)
     } else {
-      context.clearRect(this.x * 32, this.y * 32, 32, 32)
-      context.drawImage(grass, this.x * 32, this.y * 32, 32, 32)
+      mineSweeper.context.clearRect(this.x * 32, this.y * 32, 32, 32)
+      mineSweeper.context.drawImage(grass, this.x * 32, this.y * 32, 32, 32)
     }
     if (this.escavated) {
-      context.fillStyle = '#000000'
-      context.fillText(this.surroundingMines[0], this.x * 32 + 32 / 4, this.y * 32 + 32)
+      mineSweeper.context.fillStyle = '#000000'
+      mineSweeper.context.fillText(this.surroundingMines[0], this.x * 32 + 32 / 4, this.y * 32 + 32)
       if (this.isMine) {
-        context.fillStyle = '#FF0000'
-        context.fillRect(this.x * 32, this.y * 32, 32, 32)
+        mineSweeper.context.fillStyle = '#FF0000'
+        mineSweeper.context.fillRect(this.x * 32, this.y * 32, 32, 32)
       }
     }
   }
@@ -137,9 +209,36 @@ class Board {
     for (let i = 0; i < this.row; i++) {
       this.field.push([]) // creates row
       for (let j = 0; j < this.col; j++) {
-        this.field[i][j] = new Node(i, j, this.value()) // populates each row with a new Node object
+        this.field[i][j] = new Node(i, j, this.value(), this.getTile(i, j)) // populates each row with a new Node object
       }
     }
+  }
+  getTile (i, j) {
+    if (i === 0) { // are we on the left side?
+      if (j === 0) {
+        return 'topleft' // is this the top left tile?
+      }
+      if (j === this.col - 1) { // is this bottom left tile?
+        return 'botleft'
+      }
+      return 'left' // otherwise it's a left tile
+    }
+    if (j === 0) { // are we on top side?
+      if (i === this.row - 1) { // is this top right tile?
+        return 'topright'
+      }
+      return 'top'
+    }
+    if (j === this.col - 1) { // is this right side?
+      if (i === this.row - 1) { // is this bottom right tile?
+        return 'botright'
+      }
+      return 'bottom'
+    }
+    if (i === this.col - 1) {
+      return 'right'
+    }
+    return 'center'
   }
 }
 
@@ -162,7 +261,7 @@ function getMousePos (canvas, event) {
 }
 
 function moveMouse (event) {
-  let pos = getMousePos(canvas, event)
+  let pos = getMousePos(mineSweeper.canvas, event)
   if (mousePosition === null) { // has there been a previous mouse position?
     mousePosition = pos
   } else if (mousePosition.x !== pos.x || mousePosition.y !== pos.y) { // is the previous mouse position different than the current one?
@@ -175,17 +274,19 @@ function moveMouse (event) {
     oldTile.draw()
     newTile.draw()
 
+    console.log(newTile.tile)
     mousePosition = pos
   }
 }
 
 function mouseClick (event) {
-  let pos = getMousePos(canvas, event)
+  let pos = getMousePos(mineSweeper.canvas, event)
   mineSweeper.board.field[pos.x][pos.y].click()
 }
 
 window.addEventListener('load', start)
 
 function start () {
-  mineSweeper = new Minesweeper(16, 16, 50)
+  mineSweeper = new Minesweeper(15, 15, 45)
+  mineSweeper.draw()
 }
